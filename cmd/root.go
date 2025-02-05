54b494ad4da091514c6e71fc2c51b159492b480c
Daichi Hasumi <45067798+dhasumi@users.noreply.github.com>
2025-02-05 22:59:07 +0900

    docs: increments version info

--- cmd/root.go
+++ cmd/root.go
@@ -16,7 +16,7 @@ var rootCmd = &cobra.Command{
 	Use:     "jr",
 	Short:   "This is a custom Jira CLI wrapper command for personal use",
 	Long:    `This is a custom Jira CLI wrapper command for personal use.`,
-	Version: "v1.0.4",
+	Version: "v1.0.5",
 	// Uncomment the following line if your bare application
 	// has an action associated with it:
 	// Run: func(cmd *cobra.Command, args []string) {
